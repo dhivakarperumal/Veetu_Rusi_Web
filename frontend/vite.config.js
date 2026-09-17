0b10422934c97ee8d02e6c56a1357785207969da
Dhivakar <142518349+dhivakarperumal@users.noreply.github.com>
2026-09-17 12:42:57 +0530

    fix it

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000', 
-        target: 'https://veeturusi.qtechx.com',
+        target: 'http://localhost:5000', 
+        // target: 'https://veeturusi.qtechx.com',
         changeOrigin: true,
         secure: false,
       },
