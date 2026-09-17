2bf187ba839ab8264a8198d041077c69b99ac96b
qtechx001 <qtechx001@gmail.com>
2026-09-17 17:19:35 +0530

    chef sidebar

--- frontend/src/HomeChef/ChefSidebar.jsx
+++ frontend/src/HomeChef/ChefSidebar.jsx
@@ -38,7 +38,7 @@ const navItems = [
       // { path: "/chef/food/add", label: "Add Food", icon: PlusCircle },
       { path: "/chef/food/all", label: "All Food", icon: UtensilsCrossed },
       { path: "/chef/products", label: "All Products", icon: List },
-      { path: "/chef/add-products", label: "Add Product", icon: PlusSquare },
+      // { path: "/chef/add-products", label: "Add Product", icon: PlusSquare },
       { path: "/chef/products/stock", label: "Stock Details", icon: Archive }
     ]
   },
